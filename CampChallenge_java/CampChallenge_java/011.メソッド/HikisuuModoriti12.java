7097c265d47760701d7522d4f36c1dacb49a9e5f
yuujiyamagishi <iullbealright@gmail.com>
2017-11-01 18:22:35 +0900

    課題　引数と戻り値１提出します。

---
+++ CampChallenge_java/CampChallenge_java/011.メソッド/HikisuuModoriti12.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Owner
+ */
+public class HikisuuModoriti12 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            Yobidasareru y=new Yobidasareru();
+           
+         
+           
+            
+        String[] userData = y.userProfile("1");
+
+        out.print(userData);
+        }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
+class Yobidasareru {
+    String[] userProfile(String id){
+       
+        // 三人分のデータを用意する
+        String[] user1={"1","山岸","19790327","東京都"};
+        String[] user2={"2","田中","18200613","埼玉県"};
+        String[] user3={"3","佐藤","18451020","千葉県"};
+  
+        // 引数のidと比較して返すべき値を決める
+        if(id.equals("1")){
+            return user1;
+        }else if(id.equals("2")){
+            return user2;
+        }else{
+            return user3;        
+        }
+    }
+}
